157a22df0e26028be3c5c2a0dc06a22e3263a69a
phax <philip@helger.com>
2016-02-26 09:51:30 +0100

    Added another CSS hacks test file

--- src/test/java/com/helger/css/reader/CSSReader30FuncTest.java
+++ src/test/java/com/helger/css/reader/CSSReader30FuncTest.java
@@ -104,7 +104,7 @@ public final class CSSReader30FuncTest extends AbstractFuncTestCSSReader
   {
     final ECSSVersion eVersion = ECSSVersion.CSS30;
     final Charset aCharset = CCharset.CHARSET_UTF_8_OBJ;
-    final File aFile = new File ("src/test/resources/testfiles/css30/good/artificial/test-calc.css");
+    final File aFile = new File ("src/test/resources/testfiles/css30/good/artificial/hacks2.css");
     final CascadingStyleSheet aCSS = CSSReader.readFromFile (aFile, aCharset, eVersion);
     assertNotNull (aCSS);
 
