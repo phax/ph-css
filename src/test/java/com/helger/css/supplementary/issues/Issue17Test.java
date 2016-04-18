028567fe3b53ce7ff3e75cd6be5cdde19a3a2255
Philip Helger <philip@helger.com>
2016-04-18 10:46:12 +0200

    First fix for issue17 - unknown rule if no <ident> is after `@`

---
+++ src/test/java/com/helger/css/supplementary/issues/Issue17Test.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2014-2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.css.supplementary.issues;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.helger.commons.charset.CCharset;
+import com.helger.commons.io.resource.ClassPathResource;
+import com.helger.commons.io.resource.IReadableResource;
+import com.helger.css.ECSSVersion;
+import com.helger.css.decl.CascadingStyleSheet;
+import com.helger.css.reader.CSSReader;
+import com.helger.css.reader.errorhandler.LoggingCSSParseErrorHandler;
+
+/**
+ * Test for issue 17: https://github.com/phax/ph-css/issues/17
+ *
+ * @author Philip Helger
+ */
+public final class Issue17Test
+{
+  @Test
+  public void testIssue ()
+  {
+    // Multiple errors contained
+    final IReadableResource aRes = new ClassPathResource ("testfiles/css30/good/issue17.css");
+    final CascadingStyleSheet aCSS = CSSReader.readFromStream (aRes,
+                                                               CCharset.CHARSET_UTF_8_OBJ,
+                                                               ECSSVersion.CSS30,
+                                                               new LoggingCSSParseErrorHandler ());
+    assertNotNull (aCSS);
+  }
+}
