2c9a82560180a20e40a8ae0f832ef99c31d1346a
Philip Helger <philip@helger.com>
2016-09-13 16:57:11 +0200

    Media expression backport for #30

--- ph-css/src/main/java/com/helger/css/media/ECSSMediaExpressionFeature.java
+++ ph-css/src/main/java/com/helger/css/media/ECSSMediaExpressionFeature.java
@@ -66,11 +66,32 @@ public enum ECSSMediaExpressionFeature implements IHasName
   MAX_DEVICE_PIXEL_RATIO ("max-device-pixel-ratio"),
   MIN_DEVICE_PIXEL_RATIO ("min-device-pixel-ratio"),
   TRANSFORM_3D ("transform-3d"),
+  _WEBKIT_DEVICE_PIXEL_RATIO ("-webkit-device-pixel-ratio"),
   _WEBKIT_MAX_DEVICE_PIXEL_RATIO ("-webkit-max-device-pixel-ratio"),
   _WEBKIT_MIN_DEVICE_PIXEL_RATIO ("-webkit-min-device-pixel-ratio"),
   _WEBKIT_TRANSFORM_3D ("-webkit-transform-3d"),
+  _MS_HIGH_CONTRAST ("-ms-high-contrast"),
+  _MS_VIEW_STATE ("-ms-view-state"),
   _MOZ_MAX_DEVICE_PIXEL_RATIO ("-moz-max-device-pixel-ratio"),
-  _MOZ_MIN_DEVICE_PIXEL_RATIO ("-moz-min-device-pixel-ratio");
+  _MOZ_MIN_DEVICE_PIXEL_RATIO ("-moz-min-device-pixel-ratio"),
+  MAX_MOZ_DEVICE_PIXEL_RATION ("max--moz-device-pixel-ratio"),
+  MIN_MOZ_DEVICE_PIXEL_RATION ("min--moz-device-pixel-ratio"),
+  _MOZ_IMAGES_IN_MENUS ("-moz-images-in-menus"),
+  _MOZ_MAC_GRAPHITE_THEME ("-moz-mac-graphite-theme"),
+  _MOZ_MAEMO_CLASSIC ("-moz-maemo-classic"),
+  _MOZ_DEVICE_PIXEL_RATIO ("-moz-device-pixel-ratio"),
+  _MOZ_OS_VERSION ("-moz-os-version"),
+  _MOZ_SCROLLBAR_END_BACKWARD ("-moz-scrollbar-end-backward"),
+  _MOZ_SCROLLBAR_END_FORWARD ("-moz-scrollbar-end-forward"),
+  _MOZ_SCROLLBAR_START_BACKWARD ("-moz-scrollbar-start-backward"),
+  _MOZ_SCROLLBAR_START_FORWARD ("-moz-scrollbar-start-forward"),
+  _MOZ_SCROLLBAR_THUMB_PROPORTIONAL ("-moz-scrollbar-thumb-proportional"),
+  _MOZ_TOUCH_ENABLED ("-moz-touch-enabled"),
+  _MOZ_WINDOWS_CLASSIC ("-moz-windows-classic"),
+  _MOZ_WINDOWS_COMPOSITOR ("-moz-windows-compositor"),
+  _MOZ_WINDOWS_DEFAULT_THEME ("-moz-windows-default-theme"),
+  _MOZ_WINDOWS_GLASS ("-moz-windows-glass"),
+  _MOZ_WINDOWS_THEME ("-moz-windows-theme");
 
   private final String m_sName;
 
@@ -104,7 +125,16 @@ public enum ECSSMediaExpressionFeature implements IHasName
    */
   public boolean isMozillaSpecific ()
   {
-    return m_sName.startsWith ("-moz-");
+    return m_sName.contains ("-moz-");
+  }
+
+  /**
+   * @return <code>true</code> if this media expression feature is Microsoft
+   *         specific, <code>false</code> if not.
+   */
+  public boolean isMicrosoftSpecific ()
+  {
+    return m_sName.startsWith ("-ms-");
   }
 
   /**
@@ -113,7 +143,7 @@ public enum ECSSMediaExpressionFeature implements IHasName
    */
   public boolean isBrowserSpecific ()
   {
-    return m_sName.startsWith ("-");
+    return m_sName.startsWith ("-") || m_sName.contains ("--");
   }
 
   @Nullable
