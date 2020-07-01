ee74e83b3e7c330b5790c474fd47a37d90f7fdd8
Philip Helger <philip@helger.com>
2020-07-01 21:20:40 +0200

    Added test case for #61

---
+++ ph-css/src/test/java/com/helger/css/supplementary/issues/Issue61Test.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2014-2020 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.helger.css.supplementary.issues;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.helger.commons.collection.impl.ICommonsList;
+import com.helger.css.ECSSVersion;
+import com.helger.css.decl.CSSStyleRule;
+import com.helger.css.decl.ICSSTopLevelRule;
+import com.helger.css.reader.CSSReader;
+import com.helger.css.reader.CSSReaderSettings;
+
+/**
+ * Test for issue 61: https://github.com/phax/ph-css/issues/61
+ *
+ * @author Philip Helger
+ */
+public final class Issue61Test
+{
+  @Test
+  public void testIssue ()
+  {
+    final String css = ".class10 { bla:foo } .20 { bla2:foo2} .class30 { bla3:foo3 }";
+
+    final ICommonsList <ICSSTopLevelRule> aTLR = CSSReader.readFromStringReader (css,
+                                                                                 new CSSReaderSettings ().setCSSVersion (ECSSVersion.LATEST)
+                                                                                                         .setBrowserCompliantMode (true))
+                                                          .getAllRules ();
+    assertEquals (2, aTLR.size ());
+    assertTrue (aTLR.get (0) instanceof CSSStyleRule);
+    assertEquals (".class10", ((CSSStyleRule) aTLR.get (0)).getAllSelectors ().get (0).getAsCSSString ());
+    assertTrue (aTLR.get (1) instanceof CSSStyleRule);
+    assertEquals (".class30", ((CSSStyleRule) aTLR.get (1)).getAllSelectors ().get (0).getAsCSSString ());
+  }
+}
